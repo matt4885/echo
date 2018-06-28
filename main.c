0805d97297d891af50a50ad3bb71fa890de8fc51
Matthew Kempey <mkempey@gmail.com>
2018-06-28 16:56:15 -0400

    Ugly POC for passing in argv and supporting nFlag

--- main.c
+++ main.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(int argc, char *argv[]) {
+/* 
+echo 
+-------
+Prints whatever is in stdin to stdout.
+-------
+*/
+int main(int argc, char *argv[]) 
+{
+	//./echo -n hello will print hello\n
+	int nFlag = 0;
+
+	if (!strcmp(argv[1], "-n")) {
+		nFlag = 1;
+	}
+	for (int i = 1 + nFlag; i < argc; i++) {
+		for (int j = 0; j < strlen(argv[i]); j++) {
+			printf("%c", argv[i][j]);
+		}
+		printf(" ");
+	}
+
+	if (nFlag) { 
+		printf("\n");
+	}
+
 	return 0;
 }
